e7b1fef17d53a775998487d45b7c757ff93a3720
LingDong233 <82587944+LingDong233@users.noreply.github.com>
2024-12-08 01:36:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-# Добавляем данные из настроек
+# Adding data from the settings
 source ../settings.sh
 
-# Начало отсчета времени выполнения скрипта
+# Start of the script execution time
 start_time=$(date +%s)
 
-# Удаление каталога "out", если он существует
+# Delete the "out" directory if it exists
 rm -rf out
 
-# Основной каталог
-MAINPATH=/home/timisong # измените, если необходимо
-
-# Каталог ядра
+# Main Catalog
+MAINPATH=/home/lingdong # Change if necessary
+# Kernel directory
 KERNEL_DIR=$MAINPATH/kernel
 KERNEL_PATH=$KERNEL_DIR/kernel_xiaomi_sm8250
 
 git log $LAST..HEAD > ../log.txt
 BRANCH=$(git branch --show-current)
 
-# Каталоги компиляторов
+# Compiler directories
 CLANG19_DIR=$KERNEL_DIR/clang19
 ANDROID_PREBUILTS_GCC_ARM_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 ANDROID_PREBUILTS_GCC_AARCH64_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
-# Проверка и клонирование, если необходимо
+# Validation and cloning, if necessary
 check_and_clone() {
     local dir=$1
     local repo=$2
@@ -50,41 +49,41 @@ check_and_wget() {
     fi
 }
 
-# Клонирование инструментов компиляции, если они не существуют
+# Clone compilation tools if they don't exist
 check_and_wget $CLANG19_DIR https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-20240723-release/Clang-19.0.0git-20240723.tar.gz
 check_and_clone $ANDROID_PREBUILTS_GCC_ARM_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 check_and_clone $ANDROID_PREBUILTS_GCC_AARCH64_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
-# Установка переменных PATH
+# Setting PATH Variables
 PATH=$CLANG19_DIR/bin:$ANDROID_PREBUILTS_GCC_AARCH64_DIR/bin:$ANDROID_PREBUILTS_GCC_ARM_DIR/bin:$PATH
 export PATH
 export ARCH=arm64
 
-# Каталог для сборки MagicTime
+# Directory for the MagicTime build
 MAGIC_TIME_DIR="$KERNEL_DIR/MagicTime"
 
-# Создание каталога MagicTime, если его нет
+# Create a MagicTime directory if it doesn't exist
 if [ ! -d "$MAGIC_TIME_DIR" ]; then
     mkdir -p "$MAGIC_TIME_DIR"
     
-    # Проверка и клонирование Anykernel, если MagicTime не существует
+    # Verifying and Cloning Anykernel if MagicTime Doesn't Exist
     if [ ! -d "$MAGIC_TIME_DIR/Anykernel" ]; then
         git clone https://github.com/TIMISONG-dev/Anykernel.git "$MAGIC_TIME_DIR/Anykernel"
         
-        # Перемещение всех файлов из Anykernel в MagicTime
+        # Moving all files from Anykernel to MagicTime
         mv "$MAGIC_TIME_DIR/Anykernel/"* "$MAGIC_TIME_DIR/"
         
-        # Удаление папки Anykernel
+        # Deleting the Anykernel folder
         rm -rf "$MAGIC_TIME_DIR/Anykernel"
     fi
 else
-    # Если папка MagicTime существует, проверить наличие .git и удалить, если есть
+    # If the MagicTime folder exists, check for .git and delete if there is
     if [ -d "$MAGIC_TIME_DIR/.git" ]; then
         rm -rf "$MAGIC_TIME_DIR/.git"
     fi
 fi
 
-# Экспорт переменных среды
+# Export environment variables
 export IMGPATH="$MAGIC_TIME_DIR/Image"
 export DTBPATH="$MAGIC_TIME_DIR/dtb"
 export DTBOPATH="$MAGIC_TIME_DIR/dtbo.img"
@@ -94,10 +93,10 @@ export KBUILD_BUILD_USER="TIMISONG"
 export KBUILD_BUILD_HOST="timisong-dev"
 export MODEL="alioth"
 
-# Запись времени сборки
+# Build Time Recording
 MAGIC_BUILD_DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 
-# Каталог для результатов сборки
+# Catalog for build results
 output_dir=out
 
 # Конфигурация ядра
@@ -105,7 +104,7 @@ make O="$output_dir" \
             alioth_defconfig \
             vendor/xiaomi/sm8250-common.config
 
-    # Компиляция ядра
+    # Compiling the kernel
     make -j $(nproc) \
                 O="$output_dir" \
                 CC="ccache clang" \
@@ -122,18 +121,18 @@ make O="$output_dir" \
                 V=$VERBOSE 2>&1 | tee error.log
                 
 
-# Предполагается, что переменная DTS установлена ранее в скрипте
+# It is assumed that the DTS variable is set earlier in the script
 find $DTS -name '*.dtb' -exec cat {} + > $DTBPATH
 find $DTS -name 'Image' -exec cat {} + > $IMGPATH
 find $DTS -name 'dtbo.img' -exec cat {} + > $DTBOPATH
 
-# Завершение отсчета времени выполнения скрипта
+# End of the script execution time
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
 
 cd "$KERNEL_PATH"
 
-# Проверка успешности сборки
+# Checking the success of the assembly
 if grep -q -E "Ошибка 2|Error 2" error.log; then
     cd "$KERNEL_PATH"
     echo "Ошибка: Сборка завершилась с ошибкой"
@@ -148,7 +147,7 @@ if grep -q -E "Ошибка 2|Error 2" error.log; then
     -F message_thread_id="38153"
 else
     echo "Общее время выполнения: $elapsed_time секунд"
-    # Перемещение в каталог MagicTime и создание архива
+    # Moving to the MagicTime directory and creating an archive
     cd "$MAGIC_TIME_DIR"
     7z a -mx9 MagicTime-$MODEL-$MAGIC_BUILD_DATE.zip * -x!*.zip
     
